1728c3302ccf494757a7fddd1030ad5599821303
MJL7068 <mattil@netti.fi>
2018-01-25 12:14:03 +0200

    further configuring

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+from codecs import open
+
+setup(name='Ohtu-neural-networks',
+      version='0.1',
+      description='Project for Helsinki Institute of Physics, CMS upgrade project.',
+      url='https://github.com/Ohtu-project/Ohtu-neural-networks',
+      packages=find_packages(exclude=['tests'])
+      )
