b5dcfab196298d8ea36c941d812adef3e1e937a3
MJL7068 <mattil@netti.fi>
2018-01-25 12:39:34 +0200

    fixing the configuration #4

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(name='Ohtu-neural-networks',
       version='0.1',
       description='Project for Helsinki Institute of Physics, CMS upgrade project.',
       url='https://github.com/Ohtu-project/Ohtu-neural-networks',
+      author='Neural network-group',
       packages=find_packages(exclude=['tests'])
       )
