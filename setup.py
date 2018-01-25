cb571a14a9c62504016a01c229468b4874154a74
MJL7068 <mattil@netti.fi>
2018-01-25 12:42:12 +0200

    fixing the configuration #5

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(name='Ohtu-neural-networks',
       description='Project for Helsinki Institute of Physics, CMS upgrade project.',
       url='https://github.com/Ohtu-project/Ohtu-neural-networks',
       author='Neural network-group',
+      author_email='matti.leinonen@helsinki.fi',
       packages=find_packages(exclude=['tests'])
       )
