b9a9a9d6f7f25c105d3409d9203b96cabf241256
MJL7068 <mattil@netti.fi>
2018-03-09 13:08:10 +0200

    matlab integration function works

--- matlab_integration/defect_detector.m
+++ matlab_integration/defect_detector.m
@@ -1,2 +1,10 @@
+function defect_detector(path, trained_model_path, trained_model_file, image_directory_path)
+%This function goes through a directory of images and detects the defects
+%in them. The defects are saved in a csv-file.
+change_directory = strcat("cd ", path);
+system(change_directory)
 
-py.Predictions.save_prediction_to_csv();
+command = strcat("python Predictions.py ", trained_model_path + " " + trained_model_file + " " + image_directory_path);
+system(command)
+
+end
