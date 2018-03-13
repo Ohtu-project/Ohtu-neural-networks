ca2be48cbf6d2443024b691462c243b461d971da
Vitória Barin Pacela <vitoriapacela@gmail.com>
2018-03-13 17:51:35 +0200

    documented

--- count_defects.py
+++ count_defects.py
@@ -1,7 +1,12 @@
 import csv
 
-## Expects first_image_name to be in 'Acrorad_0712-1001-1-07-01028.jpg'-like format
 def next_image_name(image_name):
+    '''
+    Returns the name of the next image.
+    Expects first_image_name to be in 'Acrorad_0712-1001-1-07-01028.jpg'-like format
+    :param image_name: (str) name of the current image.
+    :return: (str) name of the next image.
+    '''
     name = image_name.split('.')[0]
     ending = image_name.split('.')[1]
     number = int(name[-4:])
@@ -19,11 +24,22 @@ def next_image_name(image_name):
     return name[:-4] + str_number + '.' + ending
 
 
-## Expects first_image_name to be in 'Acrorad_0712-1001-1-07-01028.jpg'-like format
-def count_defects (defect_labels, first_image_name, last_image_number):
+def count_defects (defect_labels, last_image_number):
+    '''
+    Returns a list containing the number of defects in each image.
+    Expects first_image_name to be in 'Acrorad_0712-1001-1-07-01028.jpg'-like format
+    To implement: Get last_image_number directly from defect_labels.
+    :param defect_labels: (list[str]) contains the image name in the second column, and the defect coordinates in the
+    third column.
+    :param last_image_number: (int) four-digit number at the end of the name of the last image from the list.
+    :return: (list)
+    '''
+    first_image_name = defect_labels[1][1] # first image in the defect_labels list
+
     defect_counts = []
-    
-    ## With different images modifications for next 3 lines are required    
+
+    # takes off the .jpg part of the name of the image
+    # and gets the four last digits of the such string
     image_number = int(first_image_name.split('.')[0][-4:])
     image_name = first_image_name
     
@@ -52,6 +68,12 @@ def count_defects (defect_labels, first_image_name, last_image_number):
 
 
 def getDefectLabels(file_name):
+    '''
+    Opens .csv file containing the defect coordinates and loads it in a list.
+    To implement: check .csv ending of file_name.
+    :param file_name: (str) name of the csv file, must contain .csv ending.
+    :return: (list) defect labels.
+    '''
     with open(file_name, 'rt') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
     
@@ -63,8 +85,12 @@ def getDefectLabels(file_name):
 
 
 def saveCount(nameToSave):
-    # nameToSave is like Acro.csv
-    # to test: check if string contains .csv in the end
+    '''
+    Saves a .csv file containing the name of the image in the first column, and the number of defects in the second
+    column.
+    To implement: add .csv ending to nameToSave if it doesn't contain it.
+    :param nameToSave: (str) name of the csv file to be saved, containing .csv ending.
+    '''
     with open(nameToSave, 'wt') as csvfile:
         writer = csv.writer(csvfile)
     
