0fc2e5f8785bfea170b7f279dba3d2b883b16ef0
EnsioS <ensiosuonpera@luukku.com>
2018-03-15 20:36:15 +0200

    function to mirror labels from csv file

---
+++ labelling_tools/generate_mirror_csv.m
@@ -0,0 +1,65 @@
+function generate_mirror_csv(file_to_read, file_to_write, image_path)
+    if ~exist('image_path','var')
+        image_path = '';
+    end
+
+    % open csv file to read annotation 
+    File = fopen(file_to_read, 'r');
+
+    % open csv file to write new content
+    % if file already exists this will override its content
+    file = fopen(file_to_write, 'w');
+
+    % read lines to sting (or char)
+    lines = textscan(File, '%s');
+    lines = lines{1};
+    fclose(File);
+
+    write_lines(file, lines, image_path, '.jpg');
+    write_lines(file, lines, image_path, 'x.jpg');
+    write_lines(file, lines, image_path, 'y.jpg');
+
+    fclose(file);
+end
+
+%% To avoid repetition
+function write_lines(file, lines, image_path, image_name_ending)
+    formatSpec1 = strcat(image_path,'%s,%i,%i,%i,%i,%s,%s\n');
+    formatSpec2 = strcat(image_path,'%s,,,,,,\n'); % for images with no defects
+
+    [n, ~] = size(lines);
+
+    for ind=1:n
+        % split line to reach numbers
+        content = strsplit(lines{ind},',');
+        
+        % generate name of image
+        image_name = content{1};
+        Cell = strsplit(image_name, '.');
+        name = strcat(image_path, Cell{1}, image_name_ending);
+        
+        if length(content) == 2  % image has no defects
+            fprintf(file, formatSpec2, name);
+        else
+            % convert to numbers
+            x1 = str2num(content{2});
+            y1 = str2num(content{3});
+            x2 = str2num(content{4});
+            y2 = str2num(content{5});
+
+            % test will coordinates be mirrored
+            if contains(image_name_ending, 'x')
+                x1 = 1280 - x2;
+                x2 = 1280 - x1;
+            elseif contains(image_name_ending, 'y')
+                y1 = 1024 - y2;
+                y2 = 1024 - y1;
+            end    
+
+            class = content{6};
+            single_or_double = content{7};
+            
+            fprintf(file, formatSpec1, name, x1, y1, x2, y2, class, single_or_double);
+        end
+    end
+end
