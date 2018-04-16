6c037a90346f0b5838af57b951161ef4c200143e
Laura Immonen <laura.e.immonen@helsinki.fi>
2018-04-16 11:57:41 +0300

    changed directories to be given as user input

--- nn/mirror.py
+++ nn/mirror.py
@@ -6,7 +6,7 @@ def _flip_x(image_path, saved_location):
     Mirror the image in horizontal direction
  
     @param image_path: The path to the image to edit
-    @param saved_location: Path to save the cropped image
+    @param saved_location: Path to save the mirrored image
     """
     image_obj = Image.open(image_path)
     rotated_image = image_obj.transpose(Image.FLIP_LEFT_RIGHT)
@@ -18,23 +18,28 @@ def _flip_y(image_path, saved_location):
     Mirror the image in vertical direction
  
     @param image_path: The path to the image to edit
-    @param saved_location: Path to save the cropped image
+    @param saved_location: Path to save the mirrored image
     """
     image_obj = Image.open(image_path)
     rotated_image = image_obj.transpose(Image.FLIP_TOP_BOTTOM)
     rotated_image.save(saved_location)
     rotated_image.show()
 
-# Read all images in directory:
+def ask_directory(text):
+    message = text
+    directory = ""
+    while not os.path.isdir(directory):
+        directory = input(message)
+        message = "There is no such directory. " + text
+    if not directory.endswith('/'):
+        directory += "/"
+    return directory
 
-# directory name
-d = '/home/barimpac/keras-retinanet/data/imgs/train/'
+d = ask_directory("Give the directory containing images you want to mirror: ")
 
 files = os.listdir(d)
 
-#to_save = '/home/barimpac/rotated/'
-
-to_save = d
+to_save = ask_directory("Give the directory where you want the mirrored images to be saved: ")
 
 for f in files:
     fx = f.replace(".jpg", "x.jpg")
