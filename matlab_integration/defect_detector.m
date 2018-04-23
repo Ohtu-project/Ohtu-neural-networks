4112226c1c6e4ceb63e6b3c0c31306acc6806b1f
MJL7068 <mattil@netti.fi>
2018-04-23 14:41:26 +0300

    more documentation

--- matlab_integration/defect_detector.m
+++ matlab_integration/defect_detector.m
@@ -4,15 +4,16 @@ function defect_detector(path, trained_model_path, trained_model_file, image_dir
 %doesn't return anything so it is a void function.
 %The function essentially just executes a system-command to run Predictions.py. Input parameters are the paths
 %to necessary files and folders.
-%parameter path is the path to Predictions.py file in string format
-%parameter trained_model_path is the path to the directory with the trained model in string format
-%parameter trained_model_file is the name of the trained model file in string format
+%parameter path is the path to matlab_integration folder in string format
+%parameter trained_model_path is the path to the the trained model file in string format
 %parameter image_directory_path is the path to the directory with the images in string format
+%parameter csv_file is the name of the resulting csv-file. This can be an empty of nonexistent file as a new csv-file is created by the program.
 
 change_directory = strcat("cd ", path);
 system(change_directory)
 
-command = strcat("python Predictions.py ", trained_model_path , " " , trained_model_file , " " + image_directory_path);
+%command = strcat("python Predictions.py ", trained_model_path , " " , trained_model_file , " " + image_directory_path);
+command = strcat("python Predictions.py ", trained_model_path, " ", image_directory_path, " ", csv_file);
 system(command)
 
 end
