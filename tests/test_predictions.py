c7a6fd7ddd0147f69ca30d36e67afa3ef2d1ac13
MJL7068 <mattil@netti.fi>
2018-04-27 13:53:19 +0300

    fixing commit #2

--- tests/test_predictions.py
+++ tests/test_predictions.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 import unittest
 import os, sys
-import mock
+from unittest import mock
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
