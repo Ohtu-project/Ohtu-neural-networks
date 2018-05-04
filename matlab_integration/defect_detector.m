772217922ed83634fbd4864e412aad1205408aa9
MJL7068 <mattil@netti.fi>
2018-05-04 18:26:37 +0300

    more documentation

--- matlab_integration/defect_detector.m
+++ matlab_integration/defect_detector.m
@@ -1,19 +1,22 @@
-function defect_detector(path, trained_model_path, image_directory_path, csv_file)
+function defect_detector(path, trained_model_path, read_image_directory_path, save_image_directory_path, csv_file)
 %This function goes through a directory of images and detects the defects
-%in them. The defects are saved in a csv-file in a same directory where Predictions.py is located. The function
-%doesn't return anything so it is a void function.
+%in them. The defects are saved in a csv-file in a same directory where Predictions.py is located.
+%Images with bounding boxes are saved in the directory determined by save_image_directory_path.
+%The function doesn't return anything so it is a void function.
 %The function essentially just executes a system-command to run Predictions.py. Input parameters are the paths
 %to necessary files and folders.
 %parameter path is the path to matlab_integration folder in string format
 %parameter trained_model_path is the path to the the trained model file in string format
-%parameter image_directory_path is the path to the directory with the images in string format
+%parameter read_image_directory_path is the path to the directory with the images in string format
+%parameter save_image_directory_path is the path to the directory where evaluated images with bounding
+%boxes are saved.
 %parameter csv_file is the name of the resulting csv-file. File of this name must not already exist
 %as a new csv-file is created by the program.
 
 change_directory = strcat("cd ", path);
 system(change_directory)
 
-command = strcat("python Predictions.py ", trained_model_path, " ", image_directory_path, " ", csv_file);
+command = strcat("python Predictions_mat.py ", trained_model_path, " ", read_image_directory_path, " ", save_image_directory_path, " ", csv_file);
 system(command)
 
 %P = py.sys.path;
--- matlab_integration/script.m
+++ matlab_integration/script.m
@@ -1,7 +1,7 @@
 path = '/home/matleino/Ohtu-neural-networks/matlab_integration/'
-trained_model_path = '/home/matleino/Ohtu-neural-networks/matlab_integration/trained-model/resnet50_csv_50_13april2018_fixed.h5'
-%trained_model_file = 'resnet50_csv_50.h5'
-image_directory_path = '/home/matleino/Ohtu-neural-networks/matlab_integration/images/'
+trained_model_path = '/home/matleino/Ohtu-neural-networks/matlab_integration/trained-model/resnet50_csv_27_inference.h5'
+read_image_directory_path = '/home/matleino/Ohtu-neural-networks/matlab_integration/images/'
+save_image_directory_path = '/home/matleino/Ohtu-neural-networks/matlab_integration/saved_images'
 csv_file = 'results.csv'
 
-defect_detector(path, trained_model_path, image_directory_path, csv_file)
+defect_detector(path, trained_model_path, read_image_directory_path, save_image_directory_path, csv_file)
